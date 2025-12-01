9be98a811a28fd5bd779000d19d961cc98264073
George <george@charge.rent>
2025-11-30 19:53:30 -0800

    bug fixes

--- src/PublicMapPage.tsx
+++ src/PublicMapPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useJsApiLoader } from "@react-google-maps/api";
 import {
   collection,
